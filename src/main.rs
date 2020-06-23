09b79d803587e7574a2ae0804ba8b97d3c16458b
Jeff Shen <jshen2014@hotmail.com>
2020-06-22 23:31:16 -0400

    added clap; basically complete!

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::time::{Instant, Duration};
 use futures::future::join_all;
 use std::fs::File;
 use std::io::prelude::*;
+use clap::{App, Arg, ArgMatches, crate_authors, crate_version, crate_description};
 
 #[derive(Deserialize, Debug)]
 struct Mirror {
@@ -41,6 +42,21 @@ struct Ranked {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let options: ArgMatches = App::new("mirrors")
+        .author(crate_authors!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .arg(Arg::with_name("fast")
+            .help("fast ranking; ranked independently of your connection speed")
+            .short("f"))
+        .arg(Arg::with_name("filename")
+            .help("name of file to write mirrorlist to")
+            .short("save")
+            .long("save")
+            .takes_value(true))
+        .get_matches();
+            
     let status_req = reqwest::get("https://www.archlinux.org/mirrors/status/json/")
         .await?
         .text()
@@ -55,51 +71,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             Some(_) => true,
             None => false,
         })
-        .map(|x| {
-            //x.url = [&x.url, "core/os/x86_64/"].join("");
-            x.details = [&x.url, "core/os/x86_64/core.db.tar.gz"].join("");
-            x
-        })
-        .collect::<Vec<_>>();
- 
-    let client = reqwest::Client::builder()
-        .connect_timeout(Duration::from_secs(15))
-        .build()?;
-
-    let waiting = servers.iter()
-        .map(|x| {
-            get_response_time(&client, &x.details)
-        })
         .collect::<Vec<_>>();
 
-    let times = join_all(waiting).await;
+    let mut ranked = if !options.is_present("fast") {
+        let client = reqwest::Client::builder()
+            .connect_timeout(Duration::from_secs(15))
+            .build()?;
+    
+        let waiting = servers.iter()
+            .map(|x| {
+                let details = [&x.url, "core/os/x86_64/core.db.tar.gz"].join("");
+                get_response_time(&client, details)
+            })
+            .collect::<Vec<_>>();
 
-    let mut ranked = (0..servers.len()).into_iter()
-        .filter_map(|i| {
-            if let Ok(time) = times[i] {
-                let url = ["Server = ", &servers[i].url, "$repo/os/$arch"].join("");
-                let score = weighted_score(servers[i].score?, (time as f64) / 1000.);
-                Some(Ranked { url, score })
-            } else {
-                None
-            }
-        })
-        .filter(|x| x.score > 0.5)
-        .collect::<Vec<_>>();
+        let times = join_all(waiting).await;
 
+    
+        (0..servers.len()).into_iter()
+            .filter_map(|i| {
+                if let Ok(time) = times[i] {
+                    let url = ["Server = ", &servers[i].url, "$repo/os/$arch"].join("");
+                    let score = weighted_score(servers[i].score?, (time as f64) / 1000.);
+                    Some(Ranked { url, score })
+                } else {
+                    None
+                }
+            })
+            .filter(|x| x.score > 0.5)
+            .collect::<Vec<_>>()
+    } else {
+        servers.into_iter()
+            .map(|x| { 
+                Ranked { url: x.url.to_string(), score: 1./x.score.unwrap() }
+            })
+            .collect::<Vec<_>>()
+    };
+    
     ranked.sort_by(|a, b| b.score.partial_cmp(&a.score).unwrap());
 
-    let strings = ranked.iter().map(|x| x.url.to_string()).collect::<Vec<_>>().join("\n");
 
-    let mut file = File::create("testing.txt")?;
-    file.write_all(strings.as_bytes())?;
+    if let Some(filename) = options.value_of("filename") {
+        let strings = ranked.iter()
+            .map(|x| x.url.to_string())
+            .collect::<Vec<_>>()
+            .join("\n");
+        let mut file = File::create(filename)?;
+        file.write_all(strings.as_bytes())?;
+    } else {
+        ranked.iter()
+            .for_each(|mirror| {
+                println!("{}", &mirror.url);
+            })
+    }
 
     Ok(())
 }
 
-async fn get_response_time(client: &reqwest::Client, url: &str) -> Result<u128, Box<dyn std::error::Error>> {
+async fn get_response_time(client: &reqwest::Client, url: String) -> Result<u128, Box<dyn std::error::Error>> {
     let now = Instant::now();
-    client.get(url).send().await?;
+    client.get(&url).send().await?;
     Ok(now.elapsed().as_millis())
 }
 
